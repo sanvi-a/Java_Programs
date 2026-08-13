cebd2584ca9a672df5922ec299436c70d404f9b8
sanvi-a <sanvia222@gmail.com>
2026-08-13 21:35:53 +0530

    Added Average of Array Elements program.

---
+++ Arrays/AverageOfArrayElements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AverageOfArrayElements {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of array elements:");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        System.out.println("The Average: " + ((double) sum / arr.length));
+    }
+}
