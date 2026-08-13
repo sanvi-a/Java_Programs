edc1b54743acf19dd3055a1c3fae3fecb7d6c584
sanvi-a <sanvia222@gmail.com>
2026-08-13 23:20:02 +0530

    Added Difference of Largest and Smallest program.

---
+++ Arrays/LargestSmallestDifference.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LargestSmallestDifference {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of array elements:");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.println("The Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        int largest = arr[0];
+        int smallest = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+        System.out.println("\nDifference: " + (largest - smallest));
+    }
+}
