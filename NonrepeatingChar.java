704582b4009d591869023470ed2148b0a7dc5de9
sanvi-a <sanvia222@gmail.com>
2026-08-13 18:47:02 +0530

    Added Non-repeating Character program.

---
+++ NonrepeatingChar.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class NonrepeatingChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String str = sc.nextLine();
+        boolean found = false;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == ch) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                System.out.println("First non-repeating character: " + ch);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("No non-repeating character found.");
+        }
+        sc.close();
+    }
+}
