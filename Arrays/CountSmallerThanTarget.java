d77ddbe0b34bfef084987623a5a42aaf6afdea87
sanvi-a <sanvia222@gmail.com>
2026-08-13 23:18:26 +0530

    Added Find Smaller than target program.

---
+++ Arrays/CountSmallerThanTarget.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CountSmallerThanTarget {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of array elements:");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.println("The Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("\nEnter the target: ");
+        int s = input.nextInt();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < s) {
+                count++;
+            }
+        }
+        System.out.printf("\nThe elements smaller that %d:%d ", s, count);
+    }
+}
