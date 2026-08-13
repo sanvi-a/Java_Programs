352a8d8aacbf3fa94861291bbc36b35989c6ba31
sanvi-a <sanvia222@gmail.com>
2026-08-14 00:44:42 +0530

    Added LinkedList Traversal program.

---
+++ LinketList/Traversal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+public class Traversal{
+    public static void main(String[] args){
+        //Creating a Linked List
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        //Connecting the nodes
+        first.next = second;
+        second.next = third;
+        Node head = first;
+        Node current = head;
+        System.out.println("Linked list elements:");
+        while(current!=null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+
+    }
+}
