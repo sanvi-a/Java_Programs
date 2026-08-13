fa38a0bcabb6381e5d041c89ee3e901ad5e1369c
sanvi-a <sanvia222@gmail.com>
2026-08-13 19:54:21 +0530

    Organize string programs

--- Strings/NonrepeatingChar.java
+++ Strings/NonrepeatingChar.java
@@ -5,23 +5,30 @@ public class NonrepeatingChar {
         System.out.print("Enter the String: ");
         String str = sc.nextLine();
         boolean found = false;
+
         for (int i = 0; i < str.length(); i++) {
+
             char ch = str.charAt(i);
             int count = 0;
+
             for (int j = 0; j < str.length(); j++) {
+
                 if (str.charAt(j) == ch) {
                     count++;
                 }
             }
+
             if (count == 1) {
                 System.out.println("First non-repeating character: " + ch);
                 found = true;
                 break;
             }
         }
+
         if (!found) {
             System.out.println("No non-repeating character found.");
         }
+
         sc.close();
     }
 }
