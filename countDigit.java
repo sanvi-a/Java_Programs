29760e2134a797ab361b346bf995e58cb1e4fc3e
sanvi-a <sanvia222@gmail.com>
2026-08-13 10:27:51 +0530

    Added Count Digit program.

---
+++ countDigit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class countDigit{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str = sc.nextLine();
+        int count = 0;
+        for(int i =0; i<str.length();i++){
+            char ch = str.charAt(i);
+            if(Character.isDigit(ch)){
+                count++;
+            }
+        }
+        System.out.println("Number of Digits: "+count);
+    }
+}
