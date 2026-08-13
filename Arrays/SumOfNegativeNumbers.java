3f3d606253ddb0dc617ee73cb7c40aed5c95d191
sanvi-a <sanvia222@gmail.com>
2026-08-13 21:48:39 +0530

    Added Sum of Negative numbers program.

---
+++ Arrays/SumOfNegativeNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumOfNegativeNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of array elements:");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0) {
+                sum += arr[i];
+            }
+        }
+        System.out.println("\nSum of negative numbers:" + sum);
+    }
+}
