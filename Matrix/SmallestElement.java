03b82ef5217d524999bb8144f64d9f5421d056ef
sanvi-a <sanvia222@gmail.com>
2026-08-18 19:51:35 +0530

    Added Smallest element of Matrix program.

---
+++ Matrix/SmallestElement.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SmallestElement{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int row = sc.nextInt();
+        System.out.println("Enter the number of columns:");
+        int column = sc.nextInt();
+        int[][] Matrix = new int[row][column];
+        System.out.println("Enter the Matrix elements:");
+        for(int i =0;i<row;i++){
+            for(int j =0;j<column;j++){
+                Matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The matrix: ");
+        for(int i =0;i<row;i++){
+            for(int j =0;j<column;j++){
+                System.out.print(Matrix[i][j]+" ");
+            }
+        }
+        int Smallest = Matrix[0][0];
+
+        for(int i =0;i<row;i++){
+            for(int j =0;j<column;j++){
+                if(Matrix[i][j]<Smallest){
+                    Smallest = Matrix[i][j];
+                }
+            }
+        }
+        System.out.println("\nSmallest Element:"+Smallest);
+    }
+}
