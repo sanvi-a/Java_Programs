1f8254048fa7afe0042ddd5f59fd420a08b8d30f
sanvi-a <sanvia222@gmail.com>
2026-08-18 20:14:53 +0530

    Added Sum of Column Matrix program.

---
+++ Matrix/ColumnSum.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ColumnSum{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the rows:");
+        int row = sc.nextInt();
+        System.out.print("Enter the column:");
+        int column = sc.nextInt();
+        int[][] Matrix = new int[row][column];
+
+        System.out.print("Enter the Matrix elements:");
+        for(int i =0;i<row;i++){
+            for(int j = 0;j<column;j++){
+                Matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matrix:");
+        for(int i =0;i<row;i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(Matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        for(int j =0;j<column;j++) {
+            int Sum =0;
+            for (int i = 0; i < row; i++) {
+                Sum += Matrix[i][j];
+            }
+            System.out.println("The Sum of Column "+ j+" is "+Sum);
+        }
+
+
+        sc.close();
+    }
+}
+
