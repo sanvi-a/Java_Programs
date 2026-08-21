36633b8cb0600a3e06bd7da5c0569c3e59e2399c
sanvi-a <sanvia222@gmail.com>
2026-08-21 22:51:40 +0530

    Added gcd finding program.

---
+++ Recursion/gcd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class gcd {
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("GCD: " + gcd(a, b));
+
+        sc.close();
+    }
+}
