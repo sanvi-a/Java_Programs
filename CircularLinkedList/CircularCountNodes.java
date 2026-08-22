a801b528d028dbb0121cf2ab76553a658658035d
sanvi-a <sanvia222@gmail.com>
2026-08-22 22:30:21 +0530

    Added Counting nodes in Circular Linked List program.

---
+++ CircularLinkedList/CircularCountNodes.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class CircularCountNodes {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element "+(i+1)+": ");
+            int data = sc.nextInt();
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+                tail.next = head;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+                tail.next = head;
+            }
+        }
+        int count = 0;
+        if (head != null) {
+            Node temp = head;
+            do {
+                count++;
+                temp = temp.next;
+            } while (temp != head);
+        }
+        System.out.println("Number of nodes: " + count);
+    }
+}
