f5b74f2a5dd1cde92570b521ba4d4ea7b717a5ee
sanvi-a <sanvia222@gmail.com>
2026-08-22 21:35:14 +0530

    Added Circular Linked List Display program.

---
+++ CircularLinkedList/CircularCreateDisplay.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class CircularCreateDisplay {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element "+(i+1)+" : ");
+            int data = sc.nextInt();
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+                tail.next = head;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+                tail.next = head;
+            }
+        }
+        System.out.println("Circular Linked List:");
+        Node temp = head;
+        if (head != null) {
+            do {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            } while (temp != head);
+        }
+    }
+}
