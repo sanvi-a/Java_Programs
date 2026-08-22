8277e8bd978439478eb5648939a53461e8b830bd
sanvi-a <sanvia222@gmail.com>
2026-08-22 22:09:54 +0530

    Added delete element in the end of Circular Linked List program.

---
+++ CircularLinkedList/CircularDeleteEnd.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class CircularDeleteEnd {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element "+(i+1)+": ");
+            int data = sc.nextInt();
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+                tail.next = head;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+                tail.next = head;
+            }
+        }
+        if (head == null) {
+            System.out.println("List is empty");
+        } else if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            Node temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            tail = temp;
+            tail.next = head;
+        }
+        System.out.println("After deletion:");
+        if (head != null) {
+            Node temp = head;
+            do {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            } while (temp != head);
+        }
+    }
+}
