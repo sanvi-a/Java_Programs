b5d61dd0850b299cb27dfec5e6b25228f8be0bf3
sanvi-a <sanvia222@gmail.com>
2026-08-22 19:06:40 +0530

    Added Reverse node traversal Linked List program.

---
+++ LinkedLists/ReverseLinkedList.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ReverseLinkedList {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            int value = sc.nextInt();
+            Node newNode = new Node(value);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        Node previous = null;
+        Node current = head;
+        while (current != null) {
+            Node next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+        System.out.print("Reversed linked list: ");
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
