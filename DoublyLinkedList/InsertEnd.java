b9aa6b961a1878c3ef91fbddf536f473a2f29de7
sanvi-a <sanvia222@gmail.com>
2026-08-22 19:44:50 +0530

    Added Doubly Linked List element at end program.

---
+++ DoublyLinkedList/InsertEnd.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class InsertEnd {
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        Node head = null;
+        // Create the original list
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element "+(i+1)+": ");
+            int data = sc.nextInt();
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+            } else {
+                Node temp = head;
+                while (temp.next != null) {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+                newNode.prev = temp;
+            }
+        }
+        // Insert at the end
+        System.out.print("Enter value to insert at end: ");
+        int value = sc.nextInt();
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            newNode.prev = temp;
+        }
+        // Display list
+        System.out.println("Doubly Linked List:");
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
