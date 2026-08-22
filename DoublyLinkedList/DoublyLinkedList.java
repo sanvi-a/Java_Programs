26fd3bdf864c661abb17cd973f92754608570ee1
sanvi-a <sanvia222@gmail.com>
2026-08-22 19:31:35 +0530

    Added Doubly Linked List program.

---
+++ DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class DoublyLinkedList {
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element "+(i+1)+": ");
+            int data = sc.nextInt();
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+            }
+        }
+        System.out.println("Forward:");
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+        System.out.println("Backward:");
+        temp = tail;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.prev;
+        }
+    }
+}
