95069008e317f8236459eb1d7f9790cb3529e2b5
sanvi-a <sanvia222@gmail.com>
2026-08-22 19:08:11 +0530

    Added Middle node Linked List program.

---
+++ LinkedLists/MiddleLinkedList.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class MiddleLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            int value = sc.nextInt();
+
+            Node newNode = new Node(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.print("Middle element: " + slow.data);
+    }
+}
