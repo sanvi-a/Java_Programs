49c9574cac9dfe9ce0981a561f1157798c3a0723
sanvi-a <sanvia222@gmail.com>
2026-08-22 20:21:27 +0530

    Added Reverse Doubly Linked List program.

---
+++ DoublyLinkedList/ReverseDoublyLinkedList.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class ReverseDoublyLinkedList {
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        Node head = null;
+        // Create the list
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element "+(i+1)+": ");
+            int data = sc.nextInt();
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+            } else {
+                Node temp = head;
+                while (temp.next != null) {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+                newNode.prev = temp;
+            }
+        }
+        // Reverse the list
+        Node current = head;
+        Node temp = null;
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+        if (temp != null) {
+            head = temp.prev;
+        }
+        // Display reversed list
+        System.out.println("Reversed Doubly Linked List:");
+        Node display = head;
+        while (display != null) {
+            System.out.print(display.data + " ");
+            display = display.next;
+        }
+    }
+}
