083bea60a205c5628fe3bb3451ee59b16e21de8f
sanvi-a <sanvia222@gmail.com>
2026-09-05 21:47:00 +0530

    Added pop element from stack program.

---
+++ Stacks/BuiltinStacks/PopElement.java
@@ -0,0 +1,18 @@
+package BuiltinStacks;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PopElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            stack.push(sc.nextInt());
+        }
+        stack.pop();
+        System.out.println("Stack after pop: " + stack);
+    }
+}
